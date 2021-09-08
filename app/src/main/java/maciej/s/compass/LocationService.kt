32eacf5cfe6448823229e5f09603c97ffa1b3d96
Maciej <betterlifethanksapp@gmail.com>
2021-09-08 22:17:55 +0200

    Create empty LocationService:Service() class and declare location permission in Manifest

---
+++ app/src/main/java/maciej/s/compass/LocationService.kt
@@ -0,0 +1,11 @@
+package maciej.s.compass
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class LocationService: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
