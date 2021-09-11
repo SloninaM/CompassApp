20fa8840401c7f7ead0c120f1d85dc7090c708e1
Maciej <betterlifethanksapp@gmail.com>
2021-09-11 14:06:36 +0200

    No more NullPointerException if your device don't have sensors

--- app/src/main/java/maciej/s/compass/MainRepository.kt
+++ app/src/main/java/maciej/s/compass/MainRepository.kt
@@ -57,7 +57,7 @@ class MainRepository {
     }
 
     fun pauseCompassSensor() {
-        compassSensorsManager!!.pauseCompassSensor()
+        compassSensorsManager?.pauseCompassSensor()
         compassSensorsManager = null
     }
 
